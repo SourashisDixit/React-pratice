d439d0ca3b979efab3f8d48f59320c6899b46620
SourashisDixit <dixitsourashis@gmail.com>
2026-08-08 00:21:34 +0530

    update2

---
+++ 01-componet/src/componet/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+  <div className='card'>
+  <h1 id='child1'>Sourashis</h1>
+  <p id='child2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ea tempora maiores eos ipsum porro nesciunt debitis magnam at beatae possimus, itaque, deserunt quis, excepturi minus eligendi! Sunt, odio nesciunt!
+  Modi architecto magnam adipisci molestias deserunt exercitationem quia corrupti libero molestiae? Quasi, odit illum dolorum, quia sint praesentium suscipit amet molestiae numquam eligendi, deserunt autem. Doloremque deleniti maiores facere voluptatum.</p>
+  
+
+  
+  </div>
+  
+ 
+  
+  )
+}
+
+export default Navbar
